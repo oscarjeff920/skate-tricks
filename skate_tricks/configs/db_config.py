32e91ba5556e3455af7cade156bda7aa85cceaa8
Oscar Jeffery <oscarjjeff920@gmail.com>
2022-02-11 13:12:18 -0300

    added database config file, alongside tests for the get_db_settings method

---
+++ skate_tricks/configs/db_config.py
@@ -0,0 +1,37 @@
+import pydantic.error_wrappers
+from pydantic import BaseSettings, PostgresDsn, validator, typing
+
+
+class DbSettings(BaseSettings):
+    DATABASE_USER: str = "postgres"
+    DATABASE_PASS: str = "postgres"
+    DATABASE_HOST: str
+    DATABASE_PORT: str
+    DATABASE_NAME: str
+    DATABASE_DSN: typing.Optional[PostgresDsn] = None
+
+    @validator("DATABASE_DSN")
+    def create_db_dsn(
+        cls, v: typing.Optional[str], values: typing.Dict[str, typing.Any]
+    ) -> str:
+        if v is None:
+            try:
+                return str(
+                    PostgresDsn.build(
+                        scheme="postgresql+psycopg2",
+                        user=values.get("DATABASE_USER", ""),
+                        password=values.get("DATABASE_PASS", ""),
+                        host=values.get("DATABASE_HOST", ""),
+                        port=values.get("DATABASE_PORT", ""),
+                        path="/{}".format(values.get("DATABASE_NAME", "")),
+                    )
+                )
+            except pydantic.error_wrappers.ValidationError as pyvaler:
+                raise pyvaler
+
+        else:
+            return v
+
+
+def get_db_settings() -> DbSettings:
+    return DbSettings()
---
+++ tests/database_test.py
@@ -0,0 +1,22 @@
+import os
+
+import pydantic.error_wrappers
+import pytest
+
+from skate_tricks.configs.db_config import get_db_settings
+
+DATABASE_DSN = "postgresql+psycopg2://postgres:postgres@10.0.0.2:5000/skate_tricks_db"
+
+
+def test_database_settings_dsn_builder() -> None:
+    os.environ["DATABASE_HOST"] = "postgres"
+    os.environ["DATABASE_PASS"] = "postgres"
+    os.environ["DATABASE_HOST"] = "10.0.0.2"
+    os.environ["DATABASE_PORT"] = "5000"
+    os.environ["DATABASE_NAME"] = "skate_tricks_db"
+    assert get_db_settings().DATABASE_DSN == DATABASE_DSN
+
+
+def test_database_settings_validation_missing_required_variables() -> None:
+    with pytest.raises(pydantic.error_wrappers.ValidationError):
+        get_db_settings().DATABASE_DSN
