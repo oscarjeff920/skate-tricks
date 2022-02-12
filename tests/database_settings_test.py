afb683cebce288bb9c794a9dde37c17c76c69d35
Oscar Jeffery <oscarjjeff920@gmail.com>
2022-02-11 21:06:51 -0300

    added a database connection test alongside a session generator. Also installed fastapi

--- tests/database_settings_test.py
+++ tests/database_settings_test.py
@@ -1,17 +1,18 @@
 import os
-
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, Session
 import pydantic.error_wrappers
 import pytest
 
-from skate_tricks.configs.db_config import get_db_settings
+from skate_tricks.configs.db_config import get_db_settings, get_db_session
 
-DATABASE_DSN = "postgresql+psycopg2://postgres:postgres@10.0.0.2:5000/skate_tricks_db"
+DATABASE_DSN = "postgresql+psycopg2://postgres:postgres@127.0.0.1:5000/skate_tricks_db"
 
 
 def test_database_settings_dsn_builder() -> None:
     os.environ["DATABASE_USER"] = "postgres"
     os.environ["DATABASE_PASS"] = "postgres"
-    os.environ["DATABASE_HOST"] = "10.0.0.2"
+    os.environ["DATABASE_HOST"] = "127.0.0.1"
     os.environ["DATABASE_PORT"] = "5000"
     os.environ["DATABASE_NAME"] = "skate_tricks_db"
     assert get_db_settings().DATABASE_DSN == DATABASE_DSN
@@ -19,4 +20,13 @@ def test_database_settings_dsn_builder() -> None:
 
 def test_database_settings_validation_missing_required_variables() -> None:
     with pytest.raises(pydantic.error_wrappers.ValidationError):
-        get_db_settings().DATABASE_DSN
+        return get_db_settings().DATABASE_DSN
+
+
+def test_database_connection() -> None:
+    engine = create_engine(DATABASE_DSN)
+    db = sessionmaker(bind=engine)()
+    try:
+        assert isinstance(db, Session)
+    finally:
+        db.close()
